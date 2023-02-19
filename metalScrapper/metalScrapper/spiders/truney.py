9d7d3c83b12688d2131447aa3cd57646e2694b62
Victor Fernandez <CubeVic@users.noreply.github.com>
2023-02-19 15:37:51 +0800

    chore: adding new hooks for code formatting and linter on pre-commit (#18)
    * chore: adding new hooks for code formating and linten on pre-commit
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- metalScrapper/metalScrapper/spiders/truney.py
+++ metalScrapper/metalScrapper/spiders/truney.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import scrapy
 from scrapy.loader import ItemLoader
 
@@ -6,17 +5,19 @@ from metalScrapper.items import Truney
 
 
 class TruneySpider(scrapy.Spider):
-    name = "truney"
-    allowed_domains = ["www.truney.com"]
-    start_urls = ["https://www.truney.com/en/shop/category/silver-2",
-                  "https://www.truney.com/en/shop/category/gold-1",
-                  "https://www.truney.com/en/shop/category/other-precious-metals-platinum-39",
-                  "https://www.truney.com/en/shop/category/other-precious-metals-palladium-40",
-                  "https://www.truney.com/en/shop/category/other-precious-metals-copper-42",
-                  "https://www.truney.com/en/shop/category/collectible-coins-30"]
+    name = 'truney'
+    allowed_domains = ['www.truney.com']
+    start_urls = [
+        'https://www.truney.com/en/shop/category/silver-2',
+        'https://www.truney.com/en/shop/category/gold-1',
+        'https://www.truney.com/en/shop/category/other-precious-metals-platinum-39',
+        'https://www.truney.com/en/shop/category/other-precious-metals-palladium-40',
+        'https://www.truney.com/en/shop/category/other-precious-metals-copper-42',
+        'https://www.truney.com/en/shop/category/collectible-coins-30',
+    ]
 
     def parse(self, response):
-        for item in response.xpath("//tbody/tr"):
+        for item in response.xpath('//tbody/tr'):
             il = ItemLoader(item=Truney(), selector=item)
             il.add_css('name', 'h6')
             il.add_css('price', 'span[data-oe-type="monetary"]>span')
@@ -24,6 +25,7 @@ class TruneySpider(scrapy.Spider):
             yield il.load_item()
 
         next_page = response.xpath(
-            '//ul[contains(@class,"pagination")]/li[last()]/a').attrib["href"]
+            '//ul[contains(@class,"pagination")]/li[last()]/a',
+        ).attrib['href']
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
